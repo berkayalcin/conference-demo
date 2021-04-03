f4e5f1656304f69d3b1fd01d9e52679db97014c6
berkay.yalcin <berkay.yalcin@trendyol.com>
2021-04-03 23:40:17 +0300

    Change packaging as war deployment

--- src/main/java/com/conference/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/conference/conferencedemo/ConferenceDemoApplication.java
@@ -2,12 +2,13 @@ package com.conference.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceDemoApplication {
+public class ConferenceDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConferenceDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceDemoApplication.class, args);
+    }
 
 }
